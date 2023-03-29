53454d411c9d0b413df60256b19c449695ac7492
NateRiz <nathan.rizik@gmail.com>
2023-03-29 11:02:10 -0700

    docs

--- NotificationService.py
+++ NotificationService.py
@@ -1,16 +1,27 @@
 import discord
 
+from Song import Song
+from SongQueue import SongQueue
+
 
 class NotificationService:
+    """Messages the channel for song and queue changes"""
     last_queue_message = None
 
     @staticmethod
-    async def notify_new_song(channel, song, song_queue):
+    async def notify_new_song(channel: discord.TextChannel, song: Song, song_queue: SongQueue) -> discord.Message:
+        """
+        Notify the channel of the newly added song and current song queue
+        :param channel: The channel to send the notification to
+        :param song: The newly added song
+        :param song_queue: The current song queue
+        :return: The message sent to the channel
+        """
         description = []
         max_fields = 8
         num_fields = min(max_fields, song_queue.length())
         for i in range(num_fields):
-            description.append(F"{i + 1}. {song_queue.get_song(i).name} {song_queue.get_song(i).duration}")
+            description.append(f"{i + 1}. {song_queue.get_song(i).name} {song_queue.get_song(i).duration}")
         if song_queue.length() > max_fields:
             description.append(f"+ {song_queue.length() - max_fields} more...")
 
@@ -25,8 +36,16 @@ class NotificationService:
         return await channel.send(embed=embed)
 
     @staticmethod
-    async def notify_queued_song(channel, song, song_queue):
+    async def notify_queued_song(channel: discord.TextChannel, song: Song, song_queue: SongQueue) -> discord.Message:
+        """
+        Notify the channel of the newly queued song and current song queue
+        :param channel: The channel to send the notification to
+        :param song: The newly queued song
+        :param song_queue: The current song queue
+        :return: The message sent to the channel
+        """
         if NotificationService.last_queue_message is not None:
             await NotificationService.last_queue_message.delete()
 
         NotificationService.last_queue_message = await NotificationService.notify_new_song(channel, song, song_queue)
+        return NotificationService.last_queue_message
--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 import asyncio
 
 import discord
+from discord import Message
 from discord.ext import commands
+from discord.ext.commands import Context
 
 from MusicPlayer import MusicPlayer
 from SECRETS import TOKEN
 
 intents = discord.Intents().all()
-client = commands.Bot(command_prefix='-', intents=intents)
+command_prefix = "-"
+client = commands.Bot(command_prefix=command_prefix, intents=intents)
 music_player = MusicPlayer(client)
 
 
 @client.event
-async def on_message(message):
-    command = message.content.split()
-    if not message.content.startswith("-"):
+async def on_message(message: Message) -> None:
+    """
+    An event that is triggered when a message is received in the server.
+    :param message: The message object that was received.
+    :return: None
+    """
+    if message.author == client.user:
         return
-    message.content = command[0].lower() + " " + " ".join(command[1:])
-    await client.process_commands(message)
+
+    if message.content.startswith(command_prefix):
+        await client.process_commands(message)
 
 
 @client.after_invoke
-async def delete_command_message(ctx):
-    # Delete the user's command message
+async def delete_command_message(ctx: Context) -> None:
+    """
+    An event that is triggered after a command is invoked. This function waits for 60 seconds,
+    then deletes the message that triggered the command.
+    :param ctx: The context object for the command.
+    :return: None
+    """
     await asyncio.sleep(60)
     await ctx.message.delete()
 
 
 @client.event
-async def on_ready():
+async def on_ready() -> None:
+    """
+    An event that is triggered when the client has successfully connected to Discord.
+    :return: None
+    """
     print("successful login as {0.user}".format(client))
 
-
-@client.command()
-async def play(ctx, *args):
+async def play(ctx: Context, *args: str) -> None:
+    """
+    A command that plays music in a voice channel.
+    :param ctx: The context object for the command.
+    :param args: The arguments passed to the command.
+    :return: None
+    """
     song_prompt = " ".join(args) if args else None
     if ctx.voice_client is None:
         if not song_prompt:
@@ -53,7 +74,15 @@ async def play(ctx, *args):
 
 
 @client.command()
-async def connect(ctx):
+async def connect(ctx: Context) -> bool:
+    """
+    A command that connects the bot to a voice channel.
+    :param ctx: The context object for the command.
+    :return: A boolean indicating if the bot successfully connected to the voice channel.
+    """
+    if ctx.voice_client is not None:
+        return False
+
     voice_state = ctx.message.author.voice
     if voice_state is not None:
         await voice_state.channel.connect()
@@ -64,7 +93,12 @@ async def connect(ctx):
 
 
 @client.command()
-async def pause(ctx):
+async def pause(ctx: Context) -> None:
+    """
+    A command that pauses the currently playing song.
+    :param ctx: The context object for the command.
+    :return: None
+    """
     voice_client = ctx.message.guild.voice_client
     if voice_client is None:
         await ctx.send("Not connected to any voice channel")
@@ -74,7 +108,12 @@ async def pause(ctx):
 
 
 @client.command()
-async def stop(ctx):
+async def stop(ctx: Context) -> None:
+    """
+    A command that stops the currently playing song and disconnects from the voice channel.
+    :param ctx: The context object for the command.
+    :return: None
+    """
     voice_client = ctx.message.guild.voice_client
     if voice_client is None:
         await ctx.send("Not connected to any voice channel")
@@ -87,13 +126,13 @@ async def stop(ctx):
     await voice_client.disconnect()
 
 
-@client.command()
-async def forceskip(ctx):
-    await skip(ctx)
-
-
-@client.command()
-async def skip(ctx):
+@client.command(aliases=["forceskip"])
+async def skip(ctx: Context) -> None:
+    """
+    A command that skips the currently playing song.
+    :param ctx: The context object for the command.
+    :return: None
+    """
     voice_client = ctx.message.guild.voice_client
     if voice_client is None:
         await ctx.send("Not connected to any voice channel")
@@ -105,8 +144,13 @@ async def skip(ctx):
         await ctx.send("The bot is not playing anything at the moment.")
 
 
-@client.command()
-async def dc(ctx):
+@client.command(aliases=["dc"])
+async def disconnect(ctx: Context) -> None:
+    """
+    A command that disconnects the bot from the voice channel.
+    :param ctx: The context object for the command.
+    :return: None
+    """
     voice_client = ctx.voice_client
     if voice_client is None:
         await ctx.send("Not connected to any voice channel")
